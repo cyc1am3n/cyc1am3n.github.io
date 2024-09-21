02a4e51df6198abd3d2c2d76e94eddcf6946f735
cyc1am3n <daeyoung.k03@gmail.com>
2024-09-22 00:12:11 +0900

    update

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -22,7 +22,10 @@ if [ -d "_scripts/publish.d" ]; then
   rm -r "/tmp/publish.d"
   cp -r "_scripts/publish.d" "/tmp/publish.d"
 fi
-bundle install
+# bundle install
+rm -r _vendor/
+cp -r ../_vendor ./_vendor
+
 bundle exec jekyll b -d /tmp/gh-pages-publish
 git ls-files -z -- . ':!:.git*' | xargs -0 rm -f
 cp -r /tmp/gh-pages-publish/* .
